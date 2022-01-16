e22cdd82315469b86e50dd469dec4d792c40ea3b
verowangari <verowangari34@gmail.com>
2022-01-16 11:15:04 +0300

    remove active onscroll in the menu and form

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ let searchfrm =document.querySelector('.head .search-bar')
 searchbtn.onclick=()=>{
     searchfrm.classList.toggle('active');
 }
+window.onscroll=() =>{
+    menu.classList.remove('active')
+    searchfrm.classList.remove('active')
+}
+
 let menu = document.querySelector('.head .navbar');
 // document.querySelector('#bars').onclick()=()=>{
 //    MediaElementAudioSourceNode.classList.toggle('active');
